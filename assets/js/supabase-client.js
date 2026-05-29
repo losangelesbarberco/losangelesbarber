dd5cdc04d2dd84b1d2e5332ae0581034afb6ed22
Maycol Sanchez Salazar <msanchez@sermepre.com>
2026-05-29 18:08:14 -0500

    Fix SyntaxError by reassigning window.supabase

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -6,6 +6,6 @@ const SUPABASE_URL = "https://jcepxgqxcokhipzzuepod.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjZXBncnhjb2toaXB6enVlcG9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwODE0MTgsImV4cCI6MjA5NTY1NzQxOH0.phS5QxY5ueEpwYzrLEjDeHw5BoVs53C-eAwcaDh9xL4";
 
 // Inicializar el cliente de Supabase
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 console.log("Supabase Client inicializado correctamente.");
